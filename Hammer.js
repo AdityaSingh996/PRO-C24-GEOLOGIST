78582109d88a108cf6698fa72fa99f5a744517ff
Aditya Singh <75933238+AdityaSingh996@users.noreply.github.com>
2021-02-15 10:29:04 +0530

    Add files via upload

---
+++ Hammer.js
@@ -0,0 +1,26 @@
+class Hammer {
+    constructor(x, y) {
+      var options = {
+          'isStatic': true,
+          
+          'restitution':0
+      }
+      
+      this.body = Bodies.rectangle(x, y,50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+   
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX 
+      pos.y = mouseY
+      rectMode(CENTER);
+      fill('black');
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
+  
